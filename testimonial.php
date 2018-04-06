98ee526bdc5fbdd4251b293a7385fd932f95505d
Adarsh Anchan <anchanadarsh@gmail.com>
2018-04-06 10:35:27 +0530

    test

---
+++ testimonial.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Nikhil Saini</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="../../../cdn-cgi/apps/head/pfH2gVrLuxujNBruafWukkTnZPE.js"></script>
+    <?php include'_partial/head.php'?>
+</head>
+
+<body>
+
+    <?php include'_partial/header.php'?>
+    <section class="mastwrap signature-adler">
+
+        <section class="first-fold signature-adler fullheight contact-bg parallax" data-stellar-background-ratio="0.5">
+            <section class="main-heading fixed-bottom">
+                <h1 class="white font3bold">Say Hello</h1>
+                <h6 class="white font3light">Get in touch with me</h6>
+                <div class="liner color-bg"></div>
+            </section>
+        </section>
+
+        <div class="inner-wrap">
+
+            <section class="hero-text signature-adler">
+                <div class="row">
+                    <article class="col-md-6">
+                        
+                    </article>
+                    <article class="col-md-6">
+
+                    </article>
+                </div>
+            </section>
+
+        </div>
+        <!-- end : inner-wrap -->
+
+        <?php include '_partial/footer.php'?>
+    </section>
+
+    <?php include '_partial/footer-scripts.php'?>
+</body>
+
+</html>
