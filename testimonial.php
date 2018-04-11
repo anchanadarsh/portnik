263263659a7474a2fe11f4a34571187734849926
Adarsh Anchan <anchanadarsh@gmail.com>
2018-04-11 12:28:16 +0530

    testimonial

--- testimonial.php
+++ testimonial.php
@@ -18,8 +18,8 @@
 
         <section class="first-fold signature-adler fullheight contact-bg parallax" data-stellar-background-ratio="0.5">
             <section class="main-heading fixed-bottom">
-                <h1 class="white font3bold">Say Hello</h1>
-                <h6 class="white font3light">Get in touch with me</h6>
+                <h1 class="white font3bold">Testimonials</h1>
+                <h6 class="white font3light">I love my clients and they love me too</h6>
                 <div class="liner color-bg"></div>
             </section>
         </section>
@@ -36,13 +36,40 @@
                                         <i class="fa fa-quote-right" aria-hidden="true"></i>
                                     </div>
                                     <div class="test_content">
-                                        <p>In publishing and graphic design, lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).</p>
+                                        <p>We found Nikhil great to work with. It’s very difficult to find someone who has equal strengths both creative and technical aspects and he utilizes them very well. very professional, quick and dedicated. We recommend him and we are looking forward to working with him again. </p>
                                     </div>
                                     <div class="test_name">
-                                        <p>Adarsh Anchan<span>Web Developer</span></p>
+                                        <p>Devendra Dhole<span>Plada technologies</span></p>
                                     </div>
                                 </div>
                             </div>
+                            <div class="col-xs-12 col-sm-6 col-md-4">
+                                <div class="testimonial-box">
+                                    <div class="test_quote">
+                                        <i class="fa fa-quote-right" aria-hidden="true"></i>
+                                    </div>
+                                    <div class="test_content">
+                                        <p>We have been associated with Nikhil for quite a while now. Honestly Speaking, Nikhil's work is quite noteworthy and totally professional. His best quality is on time performance and completion of design work to the best of his ability and totally as per Client Demands. All The Best.</p>
+                                    </div>
+                                    <div class="test_name">
+                                        <p>Samridh Vete<span>Galbao Marketing & Distribution</span></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-6 col-md-4">
+                                <div class="testimonial-box">
+                                    <div class="test_quote">
+                                        <i class="fa fa-quote-right" aria-hidden="true"></i>
+                                    </div>
+                                    <div class="test_content">
+                                        <p>Nikhil did an exceptional job on our recent startup project. Nikhil is very detail oriented, innovative, and very well illustrated our idea in a form of a logo. His creativity and understanding towards the intricacies of designing made the project all the more look good. His open-minded approach could not be overestimated as he was quite flexible to help us with our requirement. He comes out denoting a very strong experience in area of creative designing. </p>
+                                    </div>
+                                    <div class="test_name">
+                                        <p>Sakshi Gupta<span>Laundrize</span></p>
+                                    </div>
+                                </div>
+                            </div>
+<!--
                             <div class="col-xs-12 col-sm-6 col-md-4">
                                 <div class="testimonial-box">
                                     <div class="test_quote">
@@ -82,32 +109,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-xs-12 col-sm-6 col-md-4">
-                                <div class="testimonial-box">
-                                    <div class="test_quote">
-                                        <i class="fa fa-quote-right" aria-hidden="true"></i>
-                                    </div>
-                                    <div class="test_content">
-                                        <p>In publishing and graphic design, lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).</p>
-                                    </div>
-                                    <div class="test_name">
-                                        <p>Adarsh Anchan<span>Web Developer</span></p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xs-12 col-sm-6 col-md-4">
-                                <div class="testimonial-box">
-                                    <div class="test_quote">
-                                        <i class="fa fa-quote-right" aria-hidden="true"></i>
-                                    </div>
-                                    <div class="test_content">
-                                        <p>In publishing and graphic design, lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).</p>
-                                    </div>
-                                    <div class="test_name">
-                                        <p>Adarsh Anchan<span>Web Developer</span></p>
-                                    </div>
-                                </div>
-                            </div>
+-->
                         </div>
 
                     </article>
